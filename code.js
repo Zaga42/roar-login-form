1a7e4bf638c1fe1a662794c05e4e49f495b41dd4
Zaga42 <chideraokolo456@gmail.com>
2025-08-25 15:38:34 +0100

    Add files via upload

---
+++ code.js
@@ -0,0 +1,24 @@
+let bodybtn = document.getElementById("body")
+bodybtn.style.background = "linear-gradient(135deg, #0f0c29, #302b63"
+bodybtn.style.display = "flex"
+bodybtn.style.justifyContent ="center"
+bodybtn.style.alignItems = "center"
+bodybtn.style.height = "100vh"
+bodybtn.style.fontFamily ="Arial"
+
+let containerbutton = document.getElementById("container")
+containerbutton.style.background = "linear-gradient(135deg, #0f0c29, #302b63"
+containerbutton.style.padding = "55px 20px"
+containerbutton.style.borderRadius = "8px"
+containerbutton.style.border = "none"
+containerbutton.style.color = "white"
+containerbutton.style.boxShadow = "0,8px,25px rgba(0,0,0,0.4)"
+
+let submitBtneye = document.getElementById("submitBtn")
+submitBtneye.style.background = "linear-gradient(135deg, #0f0c29, #302b63"
+submitBtneye.style.color = "white"
+submitBtneye.style.border = "none"
+submitBtneye.style.borderRadius = "6px"
+submitBtneye.style.marginLeft = "80px"
+submitBtneye.style.padding = "4px 20px"
+submitBtneye.style.boxShadow = "0,8px,25px rgba(0,0,0,0.4)"
